1758f163e818ad71453e6d5798a502f134b301c7
pablitomartinez <86635786+pablitomartinez@users.noreply.github.com>
2024-07-23 01:31:51 -0300

    instal mui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { CssBaseline } from "@mui/material";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <CssBaseline />
       <body className={inter.className}>{children}</body>
     </html>
   );
